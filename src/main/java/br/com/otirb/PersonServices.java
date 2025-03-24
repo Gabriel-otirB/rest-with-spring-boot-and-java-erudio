672b4f01cc2e89aa1a529892d02d1f2bca9897a6
Gabriel-otirB <lordglayder@gmail.com>
2025-03-24 16:13:42 -0300

    Implemented findAll

--- src/main/java/br/com/otirb/PersonServices.java
+++ src/main/java/br/com/otirb/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.otirb;
 import br.com.otirb.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,16 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one Person!");
 
@@ -22,6 +34,19 @@ public class PersonServices {
         person.setLastName("Brito");
         person.setAddress("Tambaú - SP");
         person.setGender("Male");
+
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        logger.info("Finding all Persons!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Address " + i);
+        person.setGender("Gender " + i);
 
         return person;
     }
--- src/main/java/br/com/otirb/controllers/PersonController.java
+++ src/main/java/br/com/otirb/controllers/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -21,6 +23,11 @@ public class PersonController {
     )
     public Person findById(@PathVariable("id") String id) {
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
     }
 
 }
