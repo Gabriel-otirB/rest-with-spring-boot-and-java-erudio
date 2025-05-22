16232c268e041bea10129ba1a1b4ffeb2c882f67
Gabriel-otirB <lordglayder@gmail.com>
2025-05-22 16:42:06 -0300

    feature:Implementing Content Negotiation via Header Param

--- src/main/java/br/com/otirb/config/WebConfig.java
+++ src/main/java/br/com/otirb/config/WebConfig.java
@@ -13,10 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
 
         // Via EXTENSION. http://localhost:8080/api/person/v1/2.xml http://localhost:8080/api/person/v1/2.JSON Deprecated on Spring Boot 2.6
 
-        // Via QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        // Via HEADER PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
